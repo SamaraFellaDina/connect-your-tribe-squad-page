a6bb8cff4c825139378f65d73320f5abffb98123
SamaraFellaDina <sammybensink@gmail.com>
2024-02-15 16:05:31 +0100

    grid indelen

---
+++ public/script.js
@@ -0,0 +1,24 @@
+// const profileDataPlaceHolder = "assets/images/placeholder.jpg";
+// const profileData = "<%= person.avatar %>";
+
+// if (profileData) {
+//     displayImage(profileData.img);
+// } else {
+//     displayPlaceholder();
+// }
+
+
+// function displayImage(profileData) {
+//     // Get the image element
+//     const imageElement = document.getElementById('profileImage');
+//     // Set the src attribute to the profile image URL
+//     imageElement.src = profileData;
+// }
+
+
+// function displayPlaceholder() {
+//     // Get the image element
+//     const imageElement = document.getElementById('profileImage');
+//     // Set the src attribute to the placeholder image URL
+//     imageElement.src = profileDataPlaceHolder;
+// }
