45ee984e2b2d371819e79450b69a3eb9683960f8
SamaraFellaDina <sammybensink@gmail.com>
2024-02-15 22:39:40 +0100

    implenmenteren van placeholder images

--- public/script.js
+++ public/script.js
@@ -1,11 +1,4 @@
-function insertPlaceholder() {
-    const placeHolderImage = "assets/images/placeholder.jpg";
-    const imageElement = document.getElementById('profile-image');
-    
-    if (imageElement) {
-        imageElement.src = placeHolderImage; // Set src to placeholder image URL
-    }
-}
+
 
 // const elements = document.querySelectorAll('.profile li');
 // const count = elements.length;
