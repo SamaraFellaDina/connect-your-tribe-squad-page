1519fdabe21e2884bf572c372f79457affe1bc70
SamaraFellaDina <sammybensink@gmail.com>
2024-02-29 21:14:16 +0100

    css styling

--- server.js
+++ server.js
@@ -65,44 +65,53 @@ app.post('/messages/:id', function(request,response) {
 // Waarvoor is dit nodig?
 
 
-// app.post('/details/:id', function(request,response) {
-//   fetchJson(apiUrl + "/person" + request.params.id).then((apiResponse) => {
+app.post('/details/:id', function(request,response) {
+  fetchJson(apiUrl + "/person" + request.params.id).then((apiResponse) => {
 
-//   try {
-//     apiResponse.data.custom = JSON.parse(apiResponse.data.custom)
-//   } catch (e) {
-//     apiResponse.data.custom = {}
-//   }
+  try {
+    apiResponse.data.custom = JSON.parse(apiResponse.data.custom)
+  } catch (e) {
+    apiResponse.data.custom = {}
+  }
 
-//   // Stap 2: Gebruik de data uit het formulier
-//   // Deze stap zal voor iedereen net even anders zijn, afhankelijk van de functionaliteit
+  // Stap 2: Gebruik de data uit het formulier
+  // Deze stap zal voor iedereen net even anders zijn, afhankelijk van de functionaliteit
 
-//   // Controleer eerst welke actie is uitgevoerd, aan de hand van de submit button
-//   // Dit kan ook op andere manieren, of in een andere POST route
-//   if (request.body.actie == 'verstuur') {
+  // Controleer eerst welke actie is uitgevoerd, aan de hand van de submit button
+  // Dit kan ook op andere manieren, of in een andere POST route
+  if (request.body.actie == 'verstuur') {
 
-//     // Als het custom object nog geen messages Array als eigenschap heeft, voeg deze dan toe
-//     if (!apiResponse.data.custom.messages) {
-//       apiResponse.data.custom.messages = []
-//     }}
+    // Als het custom object nog geen messages Array als eigenschap heeft, voeg deze dan toe
+    if (!apiResponse.data.custom.messages) {
+      apiResponse.data.custom.messages = []
+    }
 
-//     // Voeg een nieuwe message toe voor deze persoon, aan de hand van het bericht uit het formulier
-//     apiResponse.data.custom.messages.push(request.body.message)
+    // Voeg een nieuwe message toe voor deze persoon, aan de hand van het bericht uit het formulier
+    apiResponse.data.custom.messages.push(request.body.message)
+    } else if (request.body.actie == 'vind-ik-leuk') {
 
-//     fetch(apiUrl + "/person" + request.params.id, {
-//       method: "patch",
-//       body: json.stringify({
-//         custom: apiResponse.data.custom
-//       }),
-//       headers: {
-//         'Content-type': 'application/json; charset=UTF-8'
-//       }
+      apiResponse.data.custom.like = true
+
+    } else if (request.body.actie == 'vind-ik-niet-leuk') {
+
+      apiResponse.data.custom.like = false
+
+    }
+
+    fetch(apiUrl + "/person" + request.params.id, {
+      method: "patch",
+      body: json.stringify({
+        custom: apiResponse.data.custom
+      }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8'
+      }
       
-//     }).then((patchResponse) => {
-//       response.redirect(303, '/person/' + request.params.id)
-//     })
-//   }) 
-// })
+    }).then((patchResponse) => {
+      response.redirect(303, '/person/' + request.params.id)
+    })
+  }) 
+})
 
 // Stel het poortnummer in waar express op moet gaan luisteren
 app.set('port', process.env.PORT || 8000)
